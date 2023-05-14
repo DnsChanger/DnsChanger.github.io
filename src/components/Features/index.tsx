63161c57871c108f783fca061aea1b2834ef3ae2
Mehdi Neysi <dev.mehdineysi@gmail.com>
2023-05-14 06:37:40 +0330

    refactor: make clean design for features

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -3,31 +3,21 @@ import { features } from "../../constant/features";
 
 export default function Features() {
   return (
-    <section className="bg-gray-200 py-16">
-      <div className="container mx-auto px-4">
+    <section className="bg-gray-100 py-20">
+      <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center text-gray-900 mb-8">
-          ویژگی‌های dnsChanger-desktop
+          ویژگی‌های DnsChanger Desktop
         </h2>
-        <div className="flex flex-wrap justify-center">
-          {features.map((feature: Feature) => {
-            return (
-              <div className="max-w-sm  overflow-hidden shadow-lg bg-white m-4 rounded-2xl">
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl bg-white m-4 flex items-center hover:relative hover:bottom-2">
-                  <div className="p-4">
-                    <feature.icon className="text-gray-600 text-2xl" />
-                  </div>
-                  <div className="flex-grow p-4">
-                    <div className="font-bold text-xl mb-2">
-                      {feature.title}
-                    </div>
-                    <p className="text-gray-700 text-base">
-                      {feature.description}
-                    </p>
-                  </div>
-                </div>
+        <div className="grid grid-cols-3 gap-5">
+          {features.map((feature: Feature) => (
+            <div className="max-w-sm p-6 rounded border border-gray-200 transition-all select-none hover:shadow-xl bg-white flex items-center">
+              <feature.icon className="text-gray-600 text-2xl" />
+              <div className="flex-1 p-4">
+                <div className="font-bold text-xl mb-2">{feature.title}</div>
+                <p className="text-gray-600 text-base">{feature.description}</p>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </section>
